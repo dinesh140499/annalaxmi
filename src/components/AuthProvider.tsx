69723337f61d019a1f84a66b70c5a954438379f3
dinesh kumar <officialdinesh1499@gmail.com>
2026-05-12 18:05:03 +0530

    minor change

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import { useDispatch } from "react-redux";
-import { useAuth } from "../hooks/useAuth";
-import { useEffect } from "react";
-import { setLoading, clearUser, setUser } from "../features/authSlice";
-import { useLocation } from "react-router-dom";
-
-const PUBLIC_ROUTES = ["/login", "/otp"];
-
-const AuthProvider = ({ children }: any) => {
-  const dispatch = useDispatch();
-  const location = useLocation();
-
-  const shouldFetchProfile = !PUBLIC_ROUTES.includes(location.pathname);
-  const { data, isPending, isError } = useAuth(shouldFetchProfile);
-
-  useEffect(() => {
-    dispatch(setLoading(isPending));
-
-    if (data?.user) {
-      dispatch(setUser(data));
-    }
-
-    if (isError) {
-      dispatch(clearUser());
-    }
-  }, [data, isPending, isError, dispatch]);
-=======
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth } from "../hooks/useAuth";
@@ -44,7 +16,6 @@ const AuthProvider = ({ children }: any) => {
       dispatch(logoutUser());
     }
   }, [data, isError, dispatch]);
->>>>>>> 8ae99ce3c50b4bb96ec777714b9561ebcbaacfa8
 
   return children;
 };
