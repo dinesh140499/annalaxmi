b43051c99827a4766d5527cb4679a2ec9bf7fc9d
dinesh140499 <officialdinesh1499@gmail.com>
2026-06-15 23:24:08 +0530

    category api complete

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,23 +1,98 @@
 const asyncHandler = require("../utils/asyncHandler");
 const ErrorHandler = require("../utils/errorHandler");
+const Categories = require("../models/categorySchema");
 
-exports.getCategories = asyncHandler((req, res) => {
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await Categories.find().sort({ createdAt: -1 });
+
   res.status(200).json({
     success: true,
-    message: "All Products",
+    categories,
   });
 });
 
-exports.createCategory = asyncHandler((req, res, next) => {
-  const { name, slug, image, isActice } = req.body;
+exports.getCategory = asyncHandler(async (req, res, next) => {
+  const category = await Categories.findById(req.params.id);
 
-  const fields = [name, slug, image, isActice];
-  for(let key of fields){
-    return next
+  if (!category) {
+    return next(new ErrorHandler("Category not found", 404));
   }
 
   res.status(200).json({
     success: true,
-    message: "All Products",
+    category,
+  });
+});
+
+exports.createCategory = asyncHandler(async (req, res, next) => {
+  const { name, slug } = req.body;
+
+  if (!name || !slug) {
+    return next(new ErrorHandler("All fields are required", 400));
+  }
+
+  if (!req.file) {
+    return next(new ErrorHandler("Category image is required", 400));
+  }
+
+  const existingCategory = await Categories.findOne({
+    $or: [{ name }, { slug }],
+  });
+
+  if (existingCategory) {
+    return next(new ErrorHandler("Category already exists", 400));
+  }
+
+  const category = await Categories.create({
+    name,
+    slug,
+    image: { url: req.file?.path, public_id: req.file?.filename }, // Cloudinary URL
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Category created successfully",
+    category,
+  });
+});
+
+exports.updateCategory = asyncHandler(async (req, res, next) => {
+  const { name, slug } = req.body;
+
+  const category = await Categories.findBydId(req.params.id);
+
+  if (!category) {
+    return next(new ErrorHandler("Category not found", 404));
+  }
+
+  category.name = name;
+  category.slug = slug;
+
+  if (req.file) {
+    ((category.url = req.file?.url), (category.public_id = req.file?.filename));
+  }
+
+  await category.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Category updated successfully",
+    category,
+  });
+});
+
+
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
+  const category = await Categories.findById(req.params.id);
+
+  if (!category) {
+    return next(new ErrorHandler("Category not found", 404));
+  }
+
+  await category.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Category deleted successfully",
   });
 });
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const {
   getCategories,
+  getCategory,
   createCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../controllers/category.controller");
+const { upload } = require("../middleware/upload.middleware");
+const { protect } = require("../middleware/auth.middleware");
+
 
 const router = express.Router();
 
 router.get("/", getCategories);
-router.post("/", createCategory);
+router.get("/:id", getCategory);
+
+router.post(
+  "/",
+  protect,
+  upload.single("image"),
+  createCategory
+);
+
+router.put(
+  "/:id",
+  protect,
+  upload.single("image"),
+  updateCategory
+);
+
+router.delete(
+  "/:id",
+  protect,
+  deleteCategory
+);
 
 module.exports = router;
