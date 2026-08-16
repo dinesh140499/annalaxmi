85ae92f0979e935881e86c207a9868e468ec8a3d
dinesh140499 <officialdinesh1499@gmail.com>
2026-08-16 22:36:55 +0530

    repo name change

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { get } from "../baseUrl";
 
-export const useAuth = (enabled=true) => {
+export const useAuth = (enabled = true) => {
   return useQuery({
     queryKey: ["profile"],
     queryFn: () => get("default", "user/profile"),
     retry: false,
     enabled,
     refetchOnWindowFocus: false,
-    staleTime: 5 * 60 * 1000, // cache for 5 min
+    staleTime: 5 * 60 * 1000, // cache for 5 minute
   });
 };
